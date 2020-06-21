dcebd5d595c58e3666fa2c9f1203011a278b9a6a
Manav Seksaria <ssmaanv@icloud.com>
2020-06-22 00:40:15 +0530

    DataFix

--- js/index.js
+++ js/index.js
@@ -11,38 +11,85 @@ if ( date.getHours() / 12 >= 1 ) {
 
 var projects = [
       {
-            Name: "Venugopalan Iyengar",
-            post: "President",
+            "name": "Searock 2019",
+            "month": "June",
+            "year": "2019",
+            "color": "#cd3333",
+            "code": "https://github.com/plutoniumblast/SeaRock19",
+            "org": "Department of Professional Nights",
+            "details": "My first web development scratch coded project <br> Built With: HTML, CSS, Javascript, jQuery and support libraries such as FontAwesome",
+            "link": "https://plutoniumblast.github.io/SeaRock19/",
+            "image": "https://scontent.fbom6-1.fna.fbcdn.net/v/t1.0-9/61668730_2249054108516956_4065572654633975808_o.jpg?_nc_cat=104&_nc_sid=dd9801&_nc_ohc=eCkoFRVWBeUAX-bem0P&_nc_ht=scontent.fbom6-1.fna&oh=f61fdf5a5d50d6bd3f787170310077b7&oe=5F12175B",
       },
       {
-            Name: "Vidit Parab",
-            post: "Vice President",
+            "name": "Waves 2019",
+            "month": "August",
+            "year": "2019",
+            "color": "#B45A2D",
+            "code": "https://github.com/plutoniumblast/SeaRock19",
+            "org": "Waves Controls",
+            "details": "My introduction to Node.js development frameworks <br> Built With: Vue.js, three.js, Vue Vuetify",
+            "link": "https://plutoniumblast.github.io/SeaRock19/",
+            "image": "https://www.noticebard.com/wp-content/uploads/2019/09/BITS-WAves.jpg",
+
       },
+      {
+            "name": "SEDS Celestia",
+            "month": "January",
+            "year": "2020",
+            "color": "#1d2951",
+            "code": "https://github.com/plutoniumblast/Celestia",
+            "org": "SEDS Celestia",
+            "details": "Improved version of Searock 19 with Firebase",
+            "link": "https://plutoniumblast.github.io/searock20/",
+            "image": "https://ohcampus.com/uploads/ohcampus_searock.jpg",
+      },
+      {
+            "name": "Searock 2020",
+            "month": "January",
+            "year": "2020",
+            "color": "#DCC8B4",
+            "code": "https://github.com/plutoniumblast/Abhigyaan-Website",
+            "org": "Abhigyaan",
+            "details": "Improved version of Searock 19 with Firebase",
+            "link": "http://www.abhigyaan-bpgc.in/",
+            "image": "https://ohcampus.com/uploads/ohcampus_searock.jpg",
+      },
+      {
+            "name": "Searock 2020",
+            "month": "June",
+            "year": "2019",
+            "color": "#DCC8B4",
+            "code": "https://github.com/plutoniumblast/searock20",
+            "org": "Department of Professional Nights",
+            "details": "Improved version of Searock 19 with Firebase",
+            "link": "https://plutoniumblast.github.io/searock20/",
+            "image": "https://ohcampus.com/uploads/ohcampus_searock.jpg",
+      }
 ];
 
 
 function List ( projects ) {
       return `
       <div class="sheet">
-            <div class="example-2 card">
-                  <div class="wrapper">
+      <div class="example-2 card">
+      <div class="overcard" style="color: ${projects.color };">${ projects.name }</div>
+                  <div class="wrapper" style="background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${projects.image }) center/cover no-repeat">
                         <div class="header">
                               <div class="date">
-                                    <span class="month">Aug</span>
-                                    <span class="year">2018</span>
+                                    <span class="month">${projects.month }</span>
+                                    <span class="year">${projects.year }</span>
                               </div>
-                              <ul class="menu-content">
-                                    <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
-                                    <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
+                              <ul class="menu-content"> 
+                                    <li><a href="${projects.code }" class="fa fa-code-fork"><span>Code!</span></a></li>
                               </ul>
                         </div>
                         <div class="data">
                               <div class="content">
-                                    <span class="author">BITS Pilani</span>
-                                    <h1 class="title"><a href="#">${projects.Name }</a></h1>
-                                    <p class="text">Software Used: Node.js (Vue.js), WebGL (Three.js),
-                                          Firebase, REST APIs, </p>
-                                    <a href="#" class="button">Check it Out</a>
+                                    <span class="author">${projects.org }</span>
+                                    <h1 class="title"><a href="${projects.link }">${ projects.name }</a></h1>
+                                    <p class="text">${projects.details } </p>
+                                    <a href="${projects.link }" class="button">Check it Out</a>
                               </div>
                         </div>
                   </div>
